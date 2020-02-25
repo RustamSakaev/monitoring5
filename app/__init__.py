86749cb9f63b52242d82d112750aa18980cda8f9
YourSempai256 <enot8602@gmail.com>
2020-02-25 13:18:18 +0500

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,36 @@
 from flask import Flask
+from config import Config
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
 
-from app import routes
+bootstrap = Bootstrap()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    bootstrap.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/monitoring.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Monitoring startup')
+
+    return app
